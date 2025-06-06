7ceabfdf7253e4fe4b4ee51c9ce9555c4e4dc9bc
Qerope <preloopadm@gmail.com>
2025-06-06 19:25:30 -0400

    t

--- Devoir2/next.config.mjs
+++ Devoir2/next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/SEG3525-Assignments/Devoir2/out',
-  assetPrefix: '/SEG3525-Assignments/Devoir2/out/',
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
